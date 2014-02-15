d633f2f3b1aacb522b58df49c01ba313d819964d
Daniel Wolf <danielrwolf5@gmail.com>
2014-02-15 11:48:28 -0600

    added vagrant config R6l, setting up package.json, composer.json

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -1,9 +1,11 @@
 <!doctype html>
 <html>
     <head>
-
+        <title>ChatterJS</title>
+        <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
-		<p>Here's your frontend.</p>
+        <p>Here's your frontend.</p>
+        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.12/angular.min.js"></script>
     </body>
 </html>
